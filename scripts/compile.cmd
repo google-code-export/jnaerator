06b3222abdb400f324f3c95b85e71e96b0308058
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-02-20 08:46:22 +0000

    Set javac target version to 1.5

--- scripts/compile.cmd
+++ scripts/compile.cmd
@@ -2,4 +2,4 @@ rem TO LAUNCH FROM THE trunk DIRECTORY
 
 mkdir classes 2>NUL
 
-javac -cp libraries\anarres-cpp.jar;libraries\antlr-runtime-3.1.jar;libraries\jna.jar;libraries\junit-4.4.jar;sources -d classes sources\com\ochafik\lang\jnaerator\JNAerator.java
+javac -target 1.5 -cp libraries\anarres-cpp.jar;libraries\antlr-runtime-3.1.jar;libraries\jna.jar;libraries\junit-4.4.jar;sources -d classes sources\com\ochafik\lang\jnaerator\JNAerator.java
