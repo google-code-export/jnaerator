5091db2bfeebbbd037ab96128a6c8fad20b44a02
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-03-22 01:03:27 +0000

    Fixed license notices (LGPL, not GPL ) Minor fixes in pointer conversion

--- trunk/sources/com/ochafik/lang/jnaerator/parser/ObjCppLexer.java
+++ trunk/sources/com/ochafik/lang/jnaerator/parser/ObjCppLexer.java
@@ -1,4 +1,4 @@
-// $ANTLR 3.1.2 /Users/ochafik/Prog/Java/sources/com/ochafik/lang/jnaerator/parser/ObjCpp.g 2009-03-21 02:35:28
+// $ANTLR 3.1.2 /Users/ochafik/Prog/Java/sources/com/ochafik/lang/jnaerator/parser/ObjCpp.g 2009-03-22 01:52:25
  
 /*
 	Copyright (c) 2009 Olivier Chafik, All Rights Reserved
@@ -6,16 +6,16 @@
 	This file is part of JNAerator (http://jnaerator.googlecode.com/).
 	
 	JNAerator is free software: you can redistribute it and/or modify
-	it under the terms of the GNU General Public License as published by
+	it under the terms of the GNU Lesser General Public License as published by
 	the Free Software Foundation, either version 3 of the License, or
 	(at your option) any later version.
 	
 	JNAerator is distributed in the hope that it will be useful,
 	but WITHOUT ANY WARRANTY; without even the implied warranty of
 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU General Public License for more details.
+	GNU Lesser General Public License for more details.
 	
-	You should have received a copy of the GNU General Public License
+	You should have received a copy of the GNU Lesser General Public License
 	along with JNAerator.  If not, see <http://www.gnu.org/licenses/>.
 */
 
