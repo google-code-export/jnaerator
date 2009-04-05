6d5f9469cdde29476f5ab8e3f3177c44970d39df
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-04-05 19:11:20 +0000

    Cleanup

--- sources/com/ochafik/lang/jnaerator/cplusplus/GCC4Mangler.java
+++ sources/com/ochafik/lang/jnaerator/cplusplus/GCC4Mangler.java
@@ -35,96 +35,7 @@ import com.ochafik.lang.jnaerator.parser.TypeRef.TaggedTypeRef;
 
 public class GCC4Mangler implements CPlusPlusMangler {
 	
-	/*
-	public String mangle(Method method) {
-		StringBuilder b = new StringBuilder();
-		Name n = method.getAnnotation(Name.class);
-		//Namespace ns = method.getAnnotation(Namespace.class);
-		
-		String name = n == null ? method.getName() : n.value();
-		b.append("__Z");
-		if (n.namespace() != null) {
-			b.append("N");
-			for (String e : n.namespace())
-				b.append(e.length() + e);
-			b.append(name.length() + name);
-			if (n.classMember())
-				b.append("E");
-		} else {
-			b.append(name.length() + name);
-		}
-		Type[] types = method.getGenericParameterTypes();
-		if (types.length == 0) {
-			b.append("v");
-		} else {
-			
-			Annotation[][] parameterAnnotations = method.getParameterAnnotations();
-			for (int iParam = 0; iParam < types.length; iParam++) {
-				Type type = types[iParam];
-				final Annotation[] annotations = parameterAnnotations[iParam];
-				
-				Info info = new Info(annotations);
-				if (type instanceof Class<?> && Pointer.class.isAssignableFrom((Class<?>)type)) {
-					
-				}
-				//if (Pointer.class.isAssignableFrom(type))
-			}
-		}
-		return b.toString();
-	}
-	static class Info {
-		public boolean isConst, isUnsigned, isReference;
-		Info(Annotation[] as) {
-			isConst = false;
-			isUnsigned = false;
-			isReference = false;
-			for (Annotation a : as) {
-				if (Unsigned.class.isAssignableFrom(a.getClass()))
-					isUnsigned = true;
-				else if (Const.class.isAssignableFrom(a.getClass()))
-					isConst = true;
-				else if (Reference.class.isAssignableFrom(a.getClass()))
-					isReference = true;
-			}
-		}
-	};
-	enum CPPPrim {
-		Void("v", false, null),
-		Char("c", false, Character.TYPE), 
-		UChar("h", true, Character.TYPE), 
-		Long("l", false, NativeLong.class), 
-		ULong("m", true, NativeLong.class), 
-		Int("i", false, Integer.TYPE), 
-		UInt("j", true, Integer.TYPE), 
-		Short("s", false, java.lang.Short.TYPE), 
-		UShort("t", true, java.lang.Short.TYPE), 
-		Bool("b", false, Boolean.TYPE), 
-		Float("f", false, java.lang.Float.TYPE), 
-		Double("d", false, java.lang.Double.TYPE);
-		//Pointer("P", false, com.sun.jna.Pointer.class,),
-		//Reference("R");
-		
-		final Set<Class<?>> types;
-		final boolean unsigned;
-		CPPPrim(String argMangling, boolean unsigned, Class<?>... types) {
-			this.argMangling = argMangling;
-			this.unsigned = unsigned;
-			this.types = new HashSet<Class<?>>(Arrays.asList(types));
-		}
-		
-		public Set<Class<?>> getTypes() {
-			return types;
-		}
-		public boolean isUnsigned() {
-			return unsigned;
-		}
-		String argMangling;
-		public String getArgMangling() {
-			return argMangling;
-		}
-	}*/
-
-	static void mangleArgType(TypeRef tr, StringBuilder b, Result result) {
+	protected void mangleArgType(TypeRef tr, StringBuilder b, Result result) {
 		if (tr instanceof TypeRef.TargettedTypeRef) {
 			if (tr instanceof TypeRef.Pointer && ((TypeRef.Pointer)tr).getPointerStyle() == PointerStyle.Reference)
 				b.append("R");
@@ -153,7 +64,7 @@ public class GCC4Mangler implements CPlusPlusMangler {
 			throw new UnsupportedOperationException("Cannot mangle type references of class " + tr.getClass().getName() + " : '" + tr + "'");
 		}
 	}
-	static void lenghtedName(String name, StringBuilder out) {
+	protected void lenghtedName(String name, StringBuilder out) {
 		out.append(name.length() + name);
 	}
 	public String mangle(Function function, Result result) {
@@ -164,13 +75,15 @@ public class GCC4Mangler implements CPlusPlusMangler {
 		StringBuilder b = new StringBuilder("_Z");
 		
 		Element parent = function.getParentElement();
-		List<String> ns = new ArrayList<String>(function.getNameSpace());
+		List<String> ns;
 		boolean isMethod = parent instanceof Struct;
 		if (isMethod) {
-			ns.clear();
-			ns.addAll(parent.getNameSpace());
+			ns = new ArrayList<String>(parent.getNameSpace());
 			ns.add(((Struct)parent).getTag());
+		} else {
+			ns = new ArrayList<String>(function.getNameSpace());
 		}
+		
 		if (ns.isEmpty()) {
 			lenghtedName(name, b);
 		} else {
@@ -194,17 +107,17 @@ public class GCC4Mangler implements CPlusPlusMangler {
 	}
 	static Map<Primitive, String> signatures = new HashMap<Primitive, String>();
 	static {
-		signatures.put(Primitive.Void, "v");
-		signatures.put(Primitive.Char, "c"); 
-		signatures.put(Primitive.UChar, "h"); 
-		signatures.put(Primitive.Long, "l"); 
-		signatures.put(Primitive.ULong, "m"); 
-		signatures.put(Primitive.Int, "i"); 
-		signatures.put(Primitive.UInt, "j"); 
-		signatures.put(Primitive.Short, "s"); 
-		signatures.put(Primitive.UShort, "t"); 
-		signatures.put(Primitive.Bool, "b"); 
-		signatures.put(Primitive.Float, "f"); 
-		signatures.put(Primitive.Double, "d");
+		signatures.put(Primitive.Void, 		"v");
+		signatures.put(Primitive.Char, 		"c"); 
+		signatures.put(Primitive.UChar,	 	"h"); 
+		signatures.put(Primitive.Long, 		"l"); 
+		signatures.put(Primitive.ULong,	 	"m"); 
+		signatures.put(Primitive.Int, 		"i"); 
+		signatures.put(Primitive.UInt, 		"j"); 
+		signatures.put(Primitive.Short,	 	"s"); 
+		signatures.put(Primitive.UShort, 	"t"); 
+		signatures.put(Primitive.Bool, 		"b"); 
+		signatures.put(Primitive.Float, 	"f"); 
+		signatures.put(Primitive.Double, 	"d");
 	}
 }
