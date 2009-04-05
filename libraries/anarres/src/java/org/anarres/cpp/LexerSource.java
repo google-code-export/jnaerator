cf2af7a80b6b1a755abb0308e582935c4908c74b
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-04-05 17:42:02 +0000

    merged latest changes of Anarres JCPP

--- libraries/anarres/src/java/org/anarres/cpp/LexerSource.java
+++ libraries/anarres/src/java/org/anarres/cpp/LexerSource.java
@@ -288,7 +288,7 @@ public class LexerSource extends Source {
 								LexerException {
 		int		d = read();
 		switch (d) {
-			case 'a': text.append('a'); return 0x0a;
+			case 'a': text.append('a'); return 0x07;
 			case 'b': text.append('b'); return '\b';
 			case 'f': text.append('f'); return '\f';
 			case 'n': text.append('n'); return '\n';
@@ -779,8 +779,6 @@ public class LexerSource extends Source {
 		if (tok == null) {
 			if (Character.isWhitespace(c)) {
 				tok = whitespace(c);
-				if (bol)
-					return tok;
 			}
 			else if (Character.isDigit(c)) {
 				tok = number_decimal(c);
@@ -793,7 +791,16 @@ public class LexerSource extends Source {
 			}
 		}
 
-		bol = false;
+		if (bol) {
+			switch (tok.getType()) {
+				case WHITESPACE:
+				case CCOMMENT:
+					break;
+				default:
+					bol = false;
+					break;
+			}
+		}
 
 		tok.setLocation(_l, _c);
 		if (DEBUG)
