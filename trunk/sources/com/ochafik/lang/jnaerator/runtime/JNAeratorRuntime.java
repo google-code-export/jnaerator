db4bbd5627c51a5eab29a9763cd000200090b7a1
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-04-10 01:50:05 +0000

    dummy class for rt dependencies

---
+++ trunk/sources/com/ochafik/lang/jnaerator/runtime/JNAeratorRuntime.java
@@ -0,0 +1,8 @@
+package com.ochafik.lang.jnaerator.runtime;
+
+public class JNAeratorRuntime {
+	private static final Class<?>[] dependencies = new Class<?>[] {
+		LibraryExtractor.class,
+		MangledFunctionMapper.class
+	};
+}
