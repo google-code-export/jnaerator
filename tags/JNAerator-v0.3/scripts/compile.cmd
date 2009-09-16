0a9b43d30464e1b2fd5484ba370f7109f02d3788
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-09-16 00:44:51 +0000

    removed old tags

--- tags/JNAerator-v0.3/scripts/compile.cmd
+++
@@ -1,5 +0,0 @@
-rem TO LAUNCH FROM THE trunk DIRECTORY
-
-mkdir classes 2>NUL
-
-javac -target 1.5 -cp libraries\anarres-cpp.jar;libraries\antlr-runtime-3.1.jar;libraries\jna.jar;libraries\junit-4.4.jar;sources -d classes sources\com\ochafik\lang\jnaerator\JNAerator.java
