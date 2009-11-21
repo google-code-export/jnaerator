e980a55bb027a99c236c1691e68e7f510e034b67
olivier.chafik <olivier.chafik@c48011aa-f9b7-11dd-8259-6defae454439>
2009-11-21 13:51:20 +0000

    Updated build command Removed AlreadyRetained, now included in Rococoa

--- scripts/makeJNAerator.cmd
+++ scripts/makeJNAerator.cmd
@@ -1,2 +1,5 @@
 MAVEN_OPTS=-Xmx500m
-mvn -f jnaerator-runtime/pom.xml clean deploy install && mvn "-Dstorepass=$KEYSTORE_PASS" -f jnaerator/pom.xml clean deploy site-deploy
+
+mvn -Dmaven.jar.sign.skip=false -Dstorepass=$KEYSTORE_PASS clean deploy
+
+#mvn -f jnaerator-runtime/pom.xml clean deploy install && mvn "-Dstorepass=$KEYSTORE_PASS" -f jnaerator/pom.xml clean deploy site-deploy
